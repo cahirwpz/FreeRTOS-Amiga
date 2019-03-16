654dab97c1761fb49af825f2bc6690e161304cce
Krystian Bacławski <krystian.baclawski@gmail.com>
2019-03-16 20:17:42 +0100

    Get CPU model from AmigaOS. Read VBR register on 68010.

--- build/build.vbcc.mk
+++ build/build.vbcc.mk
@@ -5,7 +5,7 @@ AS = $(PREFIX)/bin/vasm
 LD = $(PREFIX)/bin/vlink
 
 ASFLAGS = -m68010 -quiet
-CFLAGS = -c99 -cpu=68000 -O3 -size # -warn=-1 -dontwarn=307
+CFLAGS = -c99 -cpu=68010 -O3 -size # -warn=-1 -dontwarn=307
 CPPFLAGS = -I$(TOPDIR)/FreeRTOS/include \
 	   -I$(TOPDIR)/FreeRTOS/portable/VBCC/m68k \
 	   -I$(TOPDIR)/toolchain/m68k-amigaos/include \
--- startup.c
+++ startup.c
@@ -1,25 +1,28 @@
 #include <FreeRTOS.h>
 #include <hardware.h>
 #include <evec.h>
+#include <cpu.h>
 #include <libsa.h>
 
 volatile struct Custom* const custom = (APTR)0xdff000;
 
+/* Exception Vector Base: 0 for 68000, for 68010 and above read from VBR */
 ExcVec_t *ExcVecBase = (ExcVec_t *)0L;
+ExcVec_t *ReadVBR() = "\tmovec\tvbr,d0\n";
 
-extern int main(void);
+/* Value of this variable is provided by the boot loader. */
+uint8_t CpuModel = 0;
 
-__entry void _start(const HeapRegion_t *const xHeapRegions) {
+__entry void _start(uint8_t aCpuModel, const HeapRegion_t *const xHeapRegions) {
   dprintf("FreeRTOS running on Amiga!\n");
 
-  for (int i = 0; xHeapRegions[i].pucStartAddress; i++)
-    dprintf("HeapRegion[%d]: ($%08x,%d)\n", i,
-            (intptr_t)xHeapRegions[i].pucStartAddress,
-            xHeapRegions[i].xSizeInBytes);
- 
+  CpuModel = aCpuModel;
+
+  if (CpuModel & CF_68010)
+    ExcVecBase = ReadVBR();
+
   vPortDefineHeapRegions(xHeapRegions);
 
-  dprintf("FreeHeapSize: %d\n", xPortGetFreeHeapSize());
-
+  extern int main(void);
   main();
 }
