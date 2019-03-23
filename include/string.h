5712b5f580271a16cc798d559d1310ce7fa0445a
Krystian Bacławski <krystian.baclawski@gmail.com>
2019-03-23 10:15:37 +0100

    Add some extra procedures to string.h library.

--- include/string.h
+++ include/string.h
@@ -6,6 +6,13 @@
 void *memcpy(void *restrict dst, const void *restrict src, size_t n);
 void *memmove(void *dst, const void *src, size_t len);
 void *memset(void *dst, int c, size_t len);
+char *strcpy(char *dst, const char *src);
+char *strcat(char *restrict s1, const char *restrict s2);
+int strcmp(const char *s1, const char *s2);
+char *strchr(const char *s, int c);
+int strncmp(const char *s1, const char *s2, size_t n);
+char *strncpy(char *dst, const char *src, size_t len);
+char *strrchr(const char *s, int c);
 size_t strlen(const char *s);
 
 #endif /* !_STRING_H_ */
--- include/strings.h
+++ include/strings.h
@@ -4,5 +4,8 @@
 #include <stddef.h>
 
 void bzero(void *s, size_t n);
+int ffs(int value);
+char *index(const char *s, int c);
+char *rindex(const char *s, int c);
 
 #endif /* !_STRINGS_H_ */
