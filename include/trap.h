11d8d085cecb0962094695d31e9255cb71803092
Krystian Bacławski <krystian.baclawski@gmail.com>
2019-03-30 20:58:34 +0100

    Shorten trap entry code.

--- include/trap.h
+++ include/trap.h
@@ -59,9 +59,9 @@ typedef struct {
 
 #pragma pack(2)
 typedef struct TrapFrame {
-  uint16_t trapnum;
   uint32_t d0, d1, d2, d3, d4, d5, d6, d7;
   uint32_t a0, a1, a2, a3, a4, a5, a6, sp;
+  uint16_t trapnum;
   union {
     trap_stk_000_t m68000;
     trap_stk_000_memacc_t m68000_memacc;
