8fa1d90f96ff8b79138898c4947d34467f3711e0
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-03-11 20:43:57 +0100

    Add some useful C definitions.

--- include/cdefs.h
+++ include/cdefs.h
@@ -4,6 +4,34 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define abs(x)                                                                 \
+  ({                                                                           \
+    typeof(x) _x = (x);                                                        \
+    (_x < 0) ? -_x : _x;                                                       \
+  })
+
+#define min(a, b)                                                              \
+  ({                                                                           \
+    typeof(a) _a = (a);                                                        \
+    typeof(b) _b = (b);                                                        \
+    _a < _b ? _a : _b;                                                         \
+  })
+
+#define max(a, b)                                                              \
+  ({                                                                           \
+    typeof(a) _a = (a);                                                        \
+    typeof(b) _b = (b);                                                        \
+    _a > _b ? _a : _b;                                                         \
+  })
+
+#define swap(a, b)                                                             \
+  ({                                                                           \
+    typeof(a) _a = (a);                                                        \
+    typeof(a) _b = (b);                                                        \
+    (a) = _b;                                                                  \
+    (b) = _a;                                                                  \
+  })
+
 #define BIT(x) (1L << (x))
 
 #define REGB(addr) (*(volatile uint8_t *)&(addr))
@@ -14,6 +42,7 @@
 #define BSET(x, b) ((x) |= BIT(b))
 #define BCLR(x, b) ((x) &= ~BIT(b))
 
+#define __packed __attribute__((packed))
 #define __unused __attribute__((unused))
 #define __datachip __attribute__((section(".datachip")))
 #define __bsschip __attribute__((section(".bsschip")))
