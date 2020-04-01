9346b9aa19403db333248ee9a4148fcbaa1fde57
mohrcore <krzys.boronski@gmail.com>
2020-04-02 01:17:02 +0200

    fixed cia-line

--- drivers/cia-line.c
+++ drivers/cia-line.c
@@ -34,7 +34,17 @@ static void SetAlarm(uint32_t line) {
   BCLR(*ciacrb, CIACRAB_TODIN);
 }
 
+/* CIA requires the interrupt to be acknowledged by the handler.
+ * This is done by reading the value in Interrupt Control Register */
+static void AcknowledgeTimerIRQ(void) {
+	CIA_t cia = CIAB;
+	volatile uint8_t * ciab = &cia->_ciaicr;
+	uint8_t read = *ciab; 
+}
+
 static void LineCounterHandler(List_t *tasks) {
+  AcknowledgeTimerIRQ();
+
   if (listLIST_IS_EMPTY(tasks))
     return;
 
