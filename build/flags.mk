5ba3066be1cfaca33d808ed6972f561769095294
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-04-25 14:34:50 +0200

    Fail build process on a warning generated by compiler.

--- build/flags.mk
+++ build/flags.mk
@@ -2,7 +2,7 @@ ASFLAGS   += -Wa,--register-prefix-optional -Wa,--bitwise-or
 VASMFLAGS += -m68010 -quiet -I$(PREFIX)/m68k-amigaos/include
 CFLAGS    += -std=gnu11 $(OFLAGS) $(WFLAGS)
 OFLAGS    += -m68010 -Wall -Wextra -fomit-frame-pointer -Os -fno-common # -mpcrel
-WFLAGS    += -Wall -Wno-builtin-declaration-mismatch
+WFLAGS    += -Wall -Werror -Wno-builtin-declaration-mismatch
 CPPFLAGS  += -I$(TOPDIR)/FreeRTOS/portable/m68k-amiga \
 	     -I$(TOPDIR)/include \
 	     -I$(TOPDIR)
