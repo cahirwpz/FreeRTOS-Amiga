0eb8961e36efb0ee9c6a86fa3b87d5fffab88a65
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-04-27 12:42:04 +0200

    instemul: add more structure to help students next year

--- examples/instemul/main.c
+++ examples/instemul/main.c
@@ -16,8 +16,35 @@ static xTaskHandle handle;
 
 extern void vPortDefaultTrapHandler(struct TrapFrame *);
 
+/* TODO: use C bit fields to define instruction encoding */
+typedef struct MulsInst {
+} MulsInst_t;
+
+typedef struct DivsInst {
+} DivsInst_t;
+
+/* Keep in mind that:
+ * - PC & SR are placed at different positions in the trap frame
+ *   for 68000 and 68010 processors
+ * - gcc replaces 32-bit multiplication and division by calls to
+ *   __mulsi3 and __divsi3 procedures
+ */
+static bool EmulMuls(struct TrapFrame *frame) {
+  /* TODO: implement muls.l instruction emulation */
+  (void)frame;
+  return false;
+}
+
+static bool EmulDivs(struct TrapFrame *frame) {
+  /* TODO: implement divsl.l instruction emulation */
+  (void)frame;
+  return false;
+}
+
 void vPortTrapHandler(struct TrapFrame *frame) {
-  /* TODO: implement muls.l and divsl.l instruction emulation. */
+  if (EmulMuls(frame) || EmulDivs(frame))
+    return;
+
   custom.color[0] = 0xf00;
   vPortDefaultTrapHandler(frame);
 }
