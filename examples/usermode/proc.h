76d3a5309043b9c61dee5b462e75251c487fedd9
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-06-19 11:35:10 +0200

    Allow alternative way to set up user context (needed for vfork).

--- examples/usermode/proc.h
+++ examples/usermode/proc.h
@@ -6,21 +6,35 @@
 #include <setjmp.h>
 
 #define TLS_PROC 0
+#define MAXFILES 16
 
 #define UPROC_STKSZ (configMINIMAL_STACK_SIZE * 8)
 #define KPROC_STKSZ (configMINIMAL_STACK_SIZE * 2)
 
 typedef struct File File_t;
 typedef struct Hunk Hunk_t;
+typedef struct TrapFrame TrapFrame_t;
 
-#define MAXFILES 16
+/* User mode context handling stuff. */
+typedef struct UserCtx {
+  /* SR is always set to zero, no need to save it.
+   * EnterUserMode and CloneUserCtx depend on layout of this structure! */
+  uint32_t pc, sp;
+  uint32_t d0, d1, d2, d3, d4, d5, d6, d7;
+  uint32_t a0, a1, a2, a3, a4, a5, a6;
+} UserCtx_t;
 
+void CloneUserCtx(UserCtx_t *ctx, TrapFrame_t *frame);
+int EnterUserMode(UserCtx_t *ctx);
+
+/* Process control block describes process related resources. */
 typedef struct Proc {
   int pid;                 /* process identifier */
   void *ustk;              /* user stack */
   size_t ustksz;           /* size of user stack */
   int exitcode;            /* stores value from exit system call */
   jmp_buf retctx;          /* context restored when process finishes */
+  UserCtx_t usrctx;        /* initial user context */
   Hunk_t *hunk;            /* first hunk of executable file */
   File_t *fdtab[MAXFILES]; /* file descriptor table */
 } Proc_t;
@@ -32,13 +46,11 @@ void ProcInit(Proc_t *proc, size_t ustksz);
 void ProcFini(Proc_t *proc);
 int ProcLoadImage(Proc_t *proc, File_t *exe);
 void ProcFreeImage(Proc_t *proc);
-void ProcExecute(Proc_t *proc, char *const *argv);
+void ProcSetArgv(Proc_t *proc, char *const *argv);
+void ProcEnter(Proc_t *proc);
+__noreturn void ProcExit(Proc_t *proc, int exitcode);
 
 /* Descriptor table management procedures. */
 int ProcFileInstall(Proc_t *proc, int fd, File_t *file);
-
-/* Low-level internal interface. */
-int EnterUserMode(void *pc, void *sp);
-__noreturn void ExitUserMode(Proc_t *proc, int exitcode);
 
 #endif /* !_PROC_H_ */
--- examples/usermode/sysent.c
+++ examples/usermode/sysent.c
@@ -1,10 +1,17 @@
 #include <cpu.h>
 #include <trap.h>
+#include <string.h>
 
 #include "syscall.h"
 #include "proc.h"
 
 extern void vPortDefaultTrapHandler(TrapFrame_t *);
+
+void CloneUserCtx(UserCtx_t *ctx, TrapFrame_t *frame) {
+  ctx->pc = (CpuModel > CF_68000) ? frame->m68010.pc : frame->m68000.pc;
+  ctx->sp = frame->usp;
+  memcpy(&ctx->d0, &frame->d0, 15 * sizeof(uint32_t));
+}
 
 void vPortTrapHandler(TrapFrame_t *frame) {
   uint16_t sr = (CpuModel > CF_68000) ? frame->m68010.sr : frame->m68000.sr;
@@ -15,7 +22,7 @@ void vPortTrapHandler(TrapFrame_t *frame) {
 
     /* Handle known system calls! */
     if (frame->d0 == SYS_exit)
-      ExitUserMode(p, frame->d1);
+      ProcExit(p, frame->d1);
   }
 
   /* Everything else goes to default trap handler. */
