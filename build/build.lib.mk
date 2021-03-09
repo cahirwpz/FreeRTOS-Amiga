a2a32859b0774f393bf3b16e15ec791679591e87
Krystian Bacławski <krystian.baclawski@gmail.com>
2021-03-09 22:58:54 +0100

    Handle header files in all directories using build.lib.mk

--- build/build.lib.mk
+++ build/build.lib.mk
@@ -9,6 +9,8 @@ endif
 	$(AR) rcs $@ $^
 	$(RANLIB) $@
 
+SOURCES_H = $(shell find include -type f -iname '*.h')
+
 include $(TOPDIR)/build/compile.mk
 include $(TOPDIR)/build/flags.mk
 include $(TOPDIR)/build/common.mk
