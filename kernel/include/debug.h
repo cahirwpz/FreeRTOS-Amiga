bfc252747cb3ef0311c0898a7bc9455137d1de1a
Krystian Bacławski <krystian.baclawski@gmail.com>
2021-03-23 08:09:25 +0100

    Fix DASSERT & DPRINTF when DEBUG is 0

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -7,6 +7,6 @@
 #define DASSERT(x) configASSERT(x)
 #define DPRINTF(...) kprintf(__VA_ARGS__)
 #else
-#define DASSERT(x)
-#define DPRINTF(...)
+#define DASSERT(x) __nothing
+#define DPRINTF(...) __nothing
 #endif
